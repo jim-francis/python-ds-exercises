432ceedb15fca77f156ff8edcfb5b24d85fde029
Jimmy Francis <jimmyfrancis@protonmail.com>
2021-11-23 16:51:03 -0500

    Complete Exercise 22

--- 22_sum_floats/sum_floats.py
+++ 22_sum_floats/sum_floats.py
@@ -11,3 +11,16 @@ def sum_floats(nums):
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+    
+    # floaties = []
+    # for val in nums:
+    #     if isinstance(val, float):
+    #         floaties.append(val)
+    # 
+    # sum = 0
+    # for num in floaties:
+    #     sum += num
+    #     
+    # return sum
+    
+    return sum([num for num in nums if isinstance(num, float)])
