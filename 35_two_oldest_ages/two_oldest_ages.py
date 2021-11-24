20962ff591e7d2a6c1cf550391a5eadba57defb7
Jimmy Francis <jimmyfrancis@protonmail.com>
2021-11-23 19:37:43 -0500

    Complete Exercise 35

--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -21,3 +21,7 @@ def two_oldest_ages(ages):
     # you may find it helpful to research the `sorted(iter)` function, which
     # can take *any* type of list-like-thing, and returns a new, sorted list
     # from it.
+    
+    unique_ages = sorted(set(ages))
+    
+    return unique_ages[-2], unique_ages[-1]
